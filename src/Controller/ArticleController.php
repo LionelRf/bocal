e861dbe82483005f3de520c152013db333b687df
Lionel Raffin <lraffin@stagiaire-humanbooster.com>
2024-05-25 18:00:12 +0200

    bocal4

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/article/{title}', name: 'app_article')]
+    public function article(Article $article): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        return $this->render('article/article.html.twig', [
+            'article' => $article,
+            
         ]);
     }
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/index', name: 'app_index')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+        
+        
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'articles' => $articles,
         ]);
     }
 }
